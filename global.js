b32a32e6ed97984245fb8508ebc8d229ee9659a4
Garet McKinley <hello@mediachicken.com>
2017-06-03 00:35:18 -0400

    initial commit

---
+++ global.js
@@ -0,0 +1,34 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    font-family: sans-serif;
+  }
+  .inverted {
+    background-color: #1d1f27;
+    color: white;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  .header {
+    text-align: center;
+  }
+
+  .react-live-preview, .prism-code {
+    display: flex;
+    flex-grow: 1;
+    overflow: auto;
+  }
+
+  .react-live-error {
+    background: red;
+    color: white;
+    font-family: monospace;
+    white-space: pre;
+    padding: 15px;
+  }
+`;
---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import Router from 'next/router';
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from 'react-live'
+import { LayoutProvider, Section, Container, Box } from 'hedron';
+import base64 from 'base-64';
+import '../global';
+
+const code = `<LayoutProvider>
+  <Section fluid>
+    <Container hAlign="center" direction="vertical">
+      <Box className="header">
+        <img src="http://i.imgur.com/1f9IIN8.png" alt="hedron logo" />
+        <p>A no-frills flexbox grid system for React, powered by styled-components.</p>
+      </Box>
+
+      <Box width="100%" className="inverted">
+        <h2>Installation</h2>
+        <pre>npm install --save hedron@next</pre>
+      </Box>
+    </Container>
+  </Section>
+</LayoutProvider>`;
+
+class Editor extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: this.fetchCodeFromQuery() || code
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    const { query } = nextProps.url;
+    this.setState({ code: base64.decode(decodeURIComponent(query.code)) });
+  }
+  fetchCodeFromQuery() {
+    const { query } = this.props.url;
+    return query.code ? base64.decode(decodeURIComponent(query.code)) : undefined;
+  }
+  updateQuery(fromCode) {
+    Router.push(`/?code=${encodeURIComponent(base64.encode(fromCode))}`);
+  }
+  render() {
+    return (
+      <LiveProvider
+        scope={{ LayoutProvider, Section, Container, Box }}
+        code={this.state.code}
+        onUpdate={this.updateQuery}
+      >
+        <LayoutProvider gutter="0px">
+          <Section fluid>
+            <Container height="100vh">
+              <Box grow={1} width="50%">
+                <LiveEditor />
+              </Box>
+              <Box grow={1} width="50%">
+                <LiveError />
+                <LivePreview />
+              </Box>
+            </Container>
+          </Section>
+        </LayoutProvider>
+      </LiveProvider>
+    );
+  }
+}
+
+export default Editor;
