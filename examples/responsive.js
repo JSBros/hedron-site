7d42073fb956d6268db691c138f430deee2454f2
Garet McKinley <hello@mediachicken.com>
2017-06-04 02:36:12 -0400

    add sidebar example

--- examples/responsive.js
+++
@@ -1,9 +0,0 @@
-export default `<LayoutProvider debug={{ enabled: true }}>
-  <Section fluid>
-    <Container alignX="center" direction="vertical">
-      <Box className="header">
-        <p>This is the responsive example</p>
-      </Box>
-    </Container>
-  </Section>
-</LayoutProvider>`;
