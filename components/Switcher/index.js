c58f3bbd04ac41fc53002f65262655ae80167ec5
Garet McKinley <hello@mediachicken.com>
2017-06-04 01:59:23 -0400

    append additional props to end of Switcher

--- components/Switcher/index.js
+++ components/Switcher/index.js
@@ -7,17 +7,16 @@ import Wrapper from './Wrapper';
 
 const onChange = example => {
   const newExample = require(`../../examples/${example}`).default;
-  console.info(newExample)
  Router.push(`/?code=${encode(newExample)}`);
 }
 
 const Switcher = ({ ...props }) => (
   <Wrapper>
     <Dropdown
-      {...props}
       options={examples}
       onChange={e => onChange(e.value)}
       placeholder="Navigate"
+      {...props}
     />
   </Wrapper>
 );
