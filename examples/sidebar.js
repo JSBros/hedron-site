4737097380f9ad1c90f8cc3998add7ed1940889a
Garet McKinley <hello@mediachicken.com>
2017-06-04 02:39:02 -0400

    remove unused props in sidebar example

--- examples/sidebar.js
+++ examples/sidebar.js
@@ -1,7 +1,7 @@
 export default `<LayoutProvider debug={{ enabled: true }} gutter="25px">
   <Section fluid>
-    <Container height="100%" borderColor="#1194f6">
-      <Box width={{ xs: "30%" }} bg="#b3e1f7" color="#1194f6" outline>
+    <Container height="100%">
+      <Box width={{ xs: '30%' }}>
         <Container height="100%" direction="vertical" alignY="end">
           <Box>
             <h1>Hedron</h1>
@@ -9,14 +9,14 @@ export default `<LayoutProvider debug={{ enabled: true }} gutter="25px">
           </Box>
         </Container>
       </Box>
-      <Box width={{ xs: "70%" }} bg="#b3e1f7">
+      <Box width={{ xs: '70%' }}>
         <Container
           direction="vertical"
           alignY="center"
           alignX="center"
           height="100%"
         >
-          <Box bg="#b3e1f7" noOutline>
+          <Box>
             <img src="https://avatars0.githubusercontent.com/u/23329301?v=3&s=50" />
           </Box>
         </Container>
