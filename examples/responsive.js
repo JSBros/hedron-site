644b320767178e3cd0edef50178b30c2388ed8b0
Garet McKinley <hello@mediachicken.com>
2017-06-04 02:25:30 -0400

    change hAlign to alignX

--- examples/responsive.js
+++ examples/responsive.js
@@ -1,6 +1,6 @@
 export default `<LayoutProvider debug={{ enabled: true }}>
   <Section fluid>
-    <Container hAlign="center" direction="vertical">
+    <Container alignX="center" direction="vertical">
       <Box className="header">
         <p>This is the responsive example</p>
       </Box>
